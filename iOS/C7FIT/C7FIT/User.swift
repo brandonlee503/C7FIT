df39ceb524b0c76ffb4a4f3329a02547da8a848c
Brandon Lee <brandonlee503@gmail.com>
2017-01-25 21:56:20 -0800

    Clean up user model

--- iOS/C7FIT/C7FIT/User.swift
+++ iOS/C7FIT/C7FIT/User.swift
@@ -13,24 +13,48 @@ import Firebase
     A model representation of a User
  */
 struct User {
-//    let uid: String
     let email: String
-//    let name: String?
-//    let weight: Int?
-//    let height: Int?
-//    let bmi: Double?
-//    let mileTime: String?
-
-    init(authData: FIRUser) {
-//        uid = authData.uid
-        email = authData.email!
-    }
+    let photoURL: String?
+    let name: String?
+    let weight: Int?
+    let height: Int?
+    let bmi: Double?
+    let mileTime: TimeInterval?
+    let pushups: Int?
+    let situps: Int?
+    let legPress: Int?
+    let benchPress: Int?
+    let lateralPull: Int?
     
-    init(email: String) {
+    init(email: String, photoURL: String?, name: String?, weight: Int?, height: Int?, bmi: Double?, mileTime: TimeInterval?, pushups: Int?, situps: Int?, legPress: Int?, benchPress: Int?, lateralPull: Int?) {
         self.email = email
+        self.photoURL = photoURL
+        self.name = name
+        self.weight = weight
+        self.height = height
+        self.bmi = bmi
+        self.mileTime = mileTime
+        self.pushups = pushups
+        self.situps = situps
+        self.legPress = legPress
+        self.benchPress = benchPress
+        self.lateralPull = lateralPull
     }
     
-//    func toAnyObject() -> Any {
-//        return 
-//    }
+    func toAnyObject() -> Any {
+        return [
+            "email": email,
+            "photoURL": photoURL ?? "",
+            "name": name ?? "",
+            "weight": weight ?? 0,
+            "height": height ?? 0,
+            "bmi": bmi ?? 0,
+            "mileTime": mileTime ?? 0.0,
+            "pushups": pushups ?? 0,
+            "situps": situps ?? 0,
+            "legPress": legPress ?? 0,
+            "benchPress": benchPress ?? 0,
+            "lateralPull": lateralPull ?? 0
+        ]
+    }
 }
