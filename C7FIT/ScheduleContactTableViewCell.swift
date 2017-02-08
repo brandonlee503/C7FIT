f040fff6c91cfdcba3cbd76bd15b5b26322eca09
Michael Lee <WillMichael@10-248-202-101.wireless.oregonstate.edu>
2017-02-07 20:58:31 -0800

    Schedule Screen beginning layout

---
+++ C7FIT/ScheduleContactTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  ScheduleContactTableViewCell.swift
+//  C7FIT
+//
+//  Created by Michael Lee on 2/1/17.
+//  Copyright © 2017 Brandon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleContactTableViewCell: UITableViewCell {
+
+    var contactButton: UIButton = UIButton()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        print("Contact cell")
+        setup()
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    func setup(){
+        contactButton.backgroundColor = .white
+        contactButton.setTitle("Contact Us", for: .normal)
+        contactButton.setTitleColor(.black, for: .normal)
+        addSubview(contactButton)
+    }
+    
+    func setupConstraints() {
+        contactButton.translatesAutoresizingMaskIntoConstraints = false
+        let titleLead = contactButton.leftAnchor.constraint(equalTo: leftAnchor, constant:10)
+        let titleTrail = contactButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant:-10)
+        let titleTop = contactButton.topAnchor.constraint(equalTo: topAnchor, constant: 10)
+        let titleBot = contactButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        NSLayoutConstraint.activate([titleLead,titleTrail,titleTop,titleBot])
+        
+    }
+
+
+}
--- C7FIT/ScheduleViewController.swift
+++ C7FIT/ScheduleViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import MessageUI
 
-class ScheduleViewController: UIViewController {
+class ScheduleViewController: UITableViewController, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate {
     
     // MARK: - Properties
     
@@ -22,6 +23,14 @@ class ScheduleViewController: UIViewController {
         self.view.backgroundColor = .white
         self.navigationController?.navigationBar.barTintColor = .orange
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(ScheduleBrowserTableViewCell.self, forCellReuseIdentifier: "BrowserLinkCell")
+   
+        tableView.register(ScheduleBioTableViewCell.self, forCellReuseIdentifier: "BioCell")
+        tableView.register(ScheduleContactTableViewCell.self, forCellReuseIdentifier: "ContactCell")
+
+
         self.view.addSubview(scheduleView)
         setupConstraints()
         self.view.setNeedsUpdateConstraints()
@@ -32,7 +41,6 @@ class ScheduleViewController: UIViewController {
     }
     
     // MARK: - Layout
-    
     func setupConstraints() {        
         scheduleView.translatesAutoresizingMaskIntoConstraints = false
         let topView = scheduleView.topAnchor.constraint(equalTo: view.topAnchor)
@@ -41,4 +49,145 @@ class ScheduleViewController: UIViewController {
         let rightView = scheduleView.rightAnchor.constraint(equalTo: view.rightAnchor)
         NSLayoutConstraint.activate([topView, bottomView, leftView, rightView])
     }
+    
+    // MARK: UITableView Delegate and Datasource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let aspectRatioFirst: CGFloat = 1/2
+        let aspectRatioSecond: CGFloat = 1/4
+//        let aspectRatioThird: CGFloat = 1/4
+        print("loops?")
+        let screenSize : CGRect = UIScreen.main.bounds
+        //use optionals here
+        //self.navigationController?...
+        let navBarSize : CGFloat? = self.navigationController?.navigationBar.frame.size.height
+        let tabBarSize : CGFloat? = self.tabBarController?.tabBar.frame.size.height
+        if indexPath.row < 1 {
+            return aspectRatioFirst * (screenSize.height - navBarSize! - tabBarSize!)
+        } else {
+            return aspectRatioSecond * (screenSize.height - navBarSize! - tabBarSize!) //clean this up later if want different
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            if let cell: ScheduleBrowserTableViewCell = tableView.dequeueReusableCell(withIdentifier: "BrowserLinkCell") as? ScheduleBrowserTableViewCell {
+                // FIXME: Might need to update the control event
+                return cell
+            }
+        }
+        if indexPath.row == 1 {
+            if let cell: ScheduleBioTableViewCell = tableView.dequeueReusableCell(withIdentifier: "BioCell") as? ScheduleBioTableViewCell {
+                return cell
+            }
+        }
+        else if indexPath.row == 2 {
+            if let cell: ScheduleContactTableViewCell = tableView.dequeueReusableCell(withIdentifier: "ContactCell") as? ScheduleContactTableViewCell  {
+                cell.contactButton.addTarget(self, action: #selector(self.contactButtonPressed), for: .touchUpInside)
+                return cell
+            }
+        }
+        return UITableViewCell()
+    }
+    
+    
+    func contactButtonPressed(){
+        print("contact button pressed")
+        let contactGymAlert: UIAlertController = UIAlertController(title: "Contact Us", message: nil, preferredStyle: .actionSheet)
+        let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel) {
+            action -> Void in
+        }
+        let emailAction: UIAlertAction = UIAlertAction(title: "Email", style: .default, handler: { action in
+            print("email")
+            self.sendEmail()
+        })
+        let textAction: UIAlertAction = UIAlertAction(title: "Text", style: .default, handler: { action in
+            print("text")
+            self.sendText()
+        })
+        let callAction: UIAlertAction = UIAlertAction(title: "Call", style: .default, handler: { action in
+            print("Call")
+            self.callGym()
+        })
+        
+        contactGymAlert.addAction(cancelAction)
+        contactGymAlert.addAction(emailAction)
+        contactGymAlert.addAction(textAction)
+        contactGymAlert.addAction(callAction)
+
+        self.present(contactGymAlert, animated:true, completion:nil)
+    }
+    
+    func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let gymEmail = "infinitenl@gmail.com"
+            let sampleBody = "<p>Testing text</p>"
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients([gymEmail])
+            mail.setMessageBody(sampleBody, isHTML: true)
+            
+            present(mail, animated: true, completion: nil)
+        } else{
+            print(MFMailComposeViewController.canSendMail())
+            print("fail to open email box")
+        }
+    }
+    
+    func sendText() {
+        if MFMessageComposeViewController.canSendText() {
+            print("sending text")
+            let gymPhone = "229929292"
+            let sampleText = "what the"
+            let message = MFMessageComposeViewController()
+            
+            message.body = sampleText
+            message.recipients = [gymPhone]
+            message.messageComposeDelegate = self
+            present(message, animated: true, completion: nil)
+        }
+    
+    }
+
+    func callGym(){
+        let phoneNumber = "9715067484"
+        let phoneString = "tel://" + phoneNumber
+        let url = URL(string:phoneString)!
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    
+    }
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+//    func messageComposeViewController(controller: MFMessageComposeViewController!, didFinishWithResult result: MessageComposeResult) {
+//        switch (result.value) {
+//        case MessageComposeResultCancelled.value:
+//            println("Message was cancelled")
+//            self.dismissViewControllerAnimated(true, completion: nil)
+//        case MessageComposeResultFailed.value:
+//            println("Message failed")
+//            self.dismissViewControllerAnimated(true, completion: nil)
+//        case MessageComposeResultSent.value:
+//            println("Message was sent")
+//            self.dismissViewControllerAnimated(true, completion: nil)
+//        default:
+//            break;
+//        }
+//    }
+
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
 }
