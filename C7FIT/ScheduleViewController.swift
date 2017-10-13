5310e8d1f12877e226eb95005af7316a4363ea14
Brandon Lee <brandolee@ebay.com>
2017-10-13 15:30:45 -0700

    Remove extraneous cells

--- C7FIT/ScheduleViewController.swift
+++ C7FIT/ScheduleViewController.swift
@@ -54,6 +54,7 @@ class ScheduleViewController: UITableViewController, MFMailComposeViewController
         tableView.register(ScheduleCell.self, forCellReuseIdentifier: scheduleIdentifier)
         tableView.register(ClubBioCell.self, forCellReuseIdentifier: bioIdentifier)
         tableView.register(ClubContactCell.self, forCellReuseIdentifier: contactIdentifier)
+        tableView.tableFooterView = UIView()
         view.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
         view.setNeedsUpdateConstraints()
     }
