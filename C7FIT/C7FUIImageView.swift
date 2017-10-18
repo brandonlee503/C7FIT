e06fa2671e62ca335c50d638d3aaf64b34aff676
Brandon Lee <brandolee@ebay.com>
2017-10-18 10:28:24 -0700

    Add loading animation for images

--- C7FIT/C7FUIImageView.swift
+++ C7FIT/C7FUIImageView.swift
@@ -21,6 +21,15 @@ class C7FUIImageView: UIImageView {
     // MARK: - Properties
 
     var imageURLString: String?
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        [unowned self] in
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .white)
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.center = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
+        self.addSubview(activityIndicator)
+        return activityIndicator
+    }()
 
     func downloadImageFrom(urlString: String, imageMode: UIViewContentMode) {
         guard let url = URL(string: urlString) else { return }
@@ -28,6 +37,7 @@ class C7FUIImageView: UIImageView {
     }
 
     func downloadImageFrom(url: URL, imageMode: UIViewContentMode) {
+        self.activityIndicator.startAnimating()
         contentMode = imageMode
         if let cachedImage = imageCache.object(forKey: url.absoluteString as NSString) as? UIImage {
             self.image = cachedImage
@@ -35,6 +45,7 @@ class C7FUIImageView: UIImageView {
             URLSession.shared.dataTask(with: url) { data, _, error in
                 guard let data = data, error == nil else { return }
                 DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
                     if let imageToCache = UIImage(data: data) {
                         self.imageCache.setObject(imageToCache, forKey: url.absoluteString as NSString)
                         self.image = imageToCache
